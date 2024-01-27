04441e5561d4e99ea07e27bd18296edbaccb3dae
Skilledgamer101 <mansoorlunawadi@yahoo.ca>
2024-01-27 17:25:28 +0000

    fixed

--- Topic_Finder.py
+++ Topic_Finder.py
@@ -1,10 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-print(firebase_admin)
 
 cred_obj = firebase_admin.credentials.Certificate('oec-letters-firebase-adminsdk-m8ia4-f51fd3672c.json')
 default_app = firebase_admin.initialize_app(cred_obj, {
 	'databaseURL':'https://oec-letters-default-rtdb.firebaseio.com/'
 	})
 ref = db.reference("/")
+letters = ref.get()
