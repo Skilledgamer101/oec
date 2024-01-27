ac9bcdc274f2ef4321cadf66b23ee8ca981d3613
Skilledgamer101 <mansoorlunawadi@yahoo.ca>
2024-01-27 14:58:00 +0000

    l

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import text_from_link as tl
 import text_from_file as tf
 
-letter = input("Please type the letter you want to practice:  ")
+letter = input("Please type the letter you want to practice: ")
 tf.detect_document("./A.png")
 
 
 # from gcs
-tl.detect_document_uri("gs://cloud-samples-data/vision/handwriting_image.png")
+# tl.detect_document_uri("gs://cloud-samples-data/vision/handwriting_image.png")
 # from local
-tf.detect_document("./sample.png")
+# tf.detect_document("./sample.png")
 
 # doesnt work (from other web)
 # tl.detect_document_uri("https://github.com/GriDaniel/site-personal/blob/main/public/images/exp-mockup.png?raw=true")
