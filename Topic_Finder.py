bcfe036eee115e0fcc3e2be67b0cb94cd6ff000f
Skilledgamer101 <mansoorlunawadi@yahoo.ca>
2024-01-27 15:44:04 -0500

    fixed final hope

--- Topic_Finder.py
+++ Topic_Finder.py
@@ -4,8 +4,16 @@ from firebase_admin import db
 from firebase_admin import initialize_app, storage
 from google.cloud import firestore
 
+cred_obj = firebase_admin.credentials.Certificate('oec-letters-firebase-adminsdk-m8ia4-f51fd3672c.json')
+default_app = firebase_admin.initialize_app(cred_obj, {
+	'databaseURL':'https://oec-letters-default-rtdb.firebaseio.com/',
+    'storageBucket': 'oec-letters.appspot.com'})
+
+base_ref = db.reference("/")
+data_letters = db.reference("/characters")
+data_points = db.reference("/points")
+
 reverse_ref = db.reference("characters")
-print(reverse_ref)
 query=reverse_ref.order_by_child('correctness')
 results = query.get()
 
@@ -16,12 +24,3 @@ def worst_letters():
 def best_letters():
     for key in results.keys()[:5]:
         print(key)
-
-cred_obj = firebase_admin.credentials.Certificate('oec-letters-firebase-adminsdk-m8ia4-f51fd3672c.json')
-default_app = firebase_admin.initialize_app(cred_obj, {
-	'databaseURL':'https://oec-letters-default-rtdb.firebaseio.com/',
-    'storageBucket': 'oec-letters.appspot.com'})
-base_ref = db.reference("/")
-data_letters = db.reference("/characters")
-data_points = db.reference("/points")
-
