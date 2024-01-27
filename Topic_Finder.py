59eb41d43724967c1b3fe6bee59be3a0dc1eddc8
andrew <andrewchen200406@gmail.com>
2024-01-27 13:35:44 -0500

    query possibility

--- Topic_Finder.py
+++ Topic_Finder.py
@@ -2,9 +2,17 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from firebase_admin import initialize_app, storage
+from google.cloud import firestore
+
+def worst_letters():
+    reverse_ref = db.reference()
+    print(reverse_ref)
+    query=reverse_ref.order_by_child('correctness')
+    results = query.get()
+    for key,value in results.items():
+        print(key,value)
 
 print(firebase_admin)
-
 cred_obj = firebase_admin.credentials.Certificate('oec-letters-firebase-adminsdk-m8ia4-f51fd3672c.json')
 default_app = firebase_admin.initialize_app(cred_obj, {
 	'databaseURL':'https://oec-letters-default-rtdb.firebaseio.com/',
@@ -13,9 +21,14 @@ ref = db.reference("/")
 letters = ref.get()
 #Print data_base
 for key,value in letters.items():
-    print(key,value['attempts'])
+    print(key,value)
+    
+
+
+
 
 #For now
 
 print(letters['A']['attempts'])
 
+worst_letters()
