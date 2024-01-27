0f9788490f2fe2b8bb405d0649953f73c29288b9
Skilledgamer101 <mansoorlunawadi@yahoo.ca>
2024-01-27 15:56:35 +0000

    FULLY WORKING LOCAL FILE CHECK AND TELL RESULT

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import text_from_link as tl
 import text_from_file as tf
 
-letter = input("Please type the letter you want to practice: ")
-tf.detect_document("./A.png")
+#tl.detect_document_uri("gs://oec-letters.appspot.com/Screenshot (5).png")
+
+
+letters = input("Please type the letters you want to practice: ")
+file = input("Please enter the path to your handwritten letters: ")
+detected = tf.detect_document(file)
+print("")
+for letter in letters:
+    if letter in detected:
+        print(f"Nice job! You wrote {letter} correctly!\n")
+    else:
+        print(f"You might need a little practice writing {letter}\n")
 
 
 # from gcs
--- text_from_file.py
+++ text_from_file.py
@@ -9,16 +9,15 @@ def detect_document(path):
 
     image = vision.Image(content=content)
 
-    response = client.document_text_detection(image=image)
-
-    for page in response.full_text_annotation.pages:
-        for block in page.blocks:
-            for paragraph in block.paragraphs:
-                for word in paragraph.words:
-                    for symbol in word.symbols:
-                        print("\tSymbol: {}".format(symbol.text))
+    response = client.document_text_detection(image=image, image_context = {"language_hints": ["en-t-i0-handwrit"]})
     if response.error.message:
         raise Exception(
             "{}\nFor more info on error messages, check: "
             "https://cloud.google.com/apis/design/errors".format(response.error.message)
         )
+
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                for word in paragraph.words:
+                    return set([r"{}".format(symbol.text) for symbol in word.symbols])
