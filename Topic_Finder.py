99a8173ab003297889286416ba9e1a8c35212fc3
andrew <andrewchen200406@gmail.com>
2024-01-27 12:09:24 -0500

    Show be reading my screenshot

--- Topic_Finder.py
+++ Topic_Finder.py
@@ -1,12 +1,14 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+from firebase_admin import initialize_app, storage
+
 print(firebase_admin)
 
 cred_obj = firebase_admin.credentials.Certificate('oec-letters-firebase-adminsdk-m8ia4-f51fd3672c.json')
 default_app = firebase_admin.initialize_app(cred_obj, {
-	'databaseURL':'https://oec-letters-default-rtdb.firebaseio.com/'
-	})
+	'databaseURL':'https://oec-letters-default-rtdb.firebaseio.com/',
+    'storageBucket': 'oec-letters.appspot.com'})
 ref = db.reference("/")
 letters = ref.get()
 #Print data_base
@@ -14,19 +16,6 @@ for key,value in letters.items():
     print(key,value['attempts'])
 
 #For now
-for i, j in zip(range(65, 91), range(97, 123)):
-    print(chr(i),chr(j))
-    ref.update(
-        {chr(i):{
-            "attempts":0,
-             "correctness":0
-         },
-        chr(j):{
-            "attempts":0,
-            "correctness":0
-        }} 
-    )
-    
 
-
+print(letters['A']['attempts'])
 
