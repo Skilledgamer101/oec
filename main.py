a930f53b48bae3a3ef4eaa346e8091fb8bd8f029
Skilledgamer101 <mansoorlunawadi@yahoo.ca>
2024-01-27 14:44:31 +0000

    working

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import text_from_link as tl
 import text_from_file as tf
 
+# from gcs
 tl.detect_document_uri("gs://cloud-samples-data/vision/handwriting_image.png")
-tf.detect_document("./sample.img")
+# from local
+tf.detect_document("./sample.png")
+
+# doesnt work (from other web)
+# tl.detect_document_uri("https://github.com/GriDaniel/site-personal/blob/main/public/images/exp-mockup.png?raw=true")
